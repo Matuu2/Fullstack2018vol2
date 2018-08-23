0e1b2c900828a3ab92011873a0ae4274f60ac27d
Matias Ala-Sunila <matuu2@hotmail.fi>
2018-08-23 20:56:52 +0300

    Tehty 3.1

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+let notes = [
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Martti Tienari', number: '040-123456' },
+    { name: 'Arto Järvinen', number: '040-123456' },
+    { name: 'Lea Kutvonen', number: '040-123456' }
+]
+
+app.get('/api/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(notes)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
