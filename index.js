8c4ea7b1a57d5e493d3181aadef408d8f52d7f0f
Matias Ala-Sunila <matuu2@hotmail.fi>
2018-08-24 12:08:29 +0300

    Tehty 3.2-3.6

--- index.js
+++ index.js
@@ -1,19 +1,63 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
-let notes = [
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Martti Tienari', number: '040-123456' },
-    { name: 'Arto Järvinen', number: '040-123456' },
-    { name: 'Lea Kutvonen', number: '040-123456' }
+let persons = [
+    { name: 'Arto Hellas', number: '040-123456' ,id: 0},
+    { name: 'Martti Tienari', number: '040-123456' ,id: 1},
+    { name: 'Arto Järvinen', number: '040-123456' ,id: 2},
+    { name: 'Lea Kutvonen', number: '040-123456' ,id: 3}
 ]
+
+app.use(bodyParser.json())
 
 app.get('/api/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(notes)
+  res.json(persons)
+})
+
+app.get('/api/info', (req, res) => {
+  const time = new Date()
+  const count = Object.keys(persons).length 
+  res.send(`<p>Puhelinluettelossa on ${count} henkilön tiedot </p>`+
+    `<p>${time}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if ( person ) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons/', (request, response) => {
+  if (request.body.name === undefined) {
+    return response.status(400).json({error: 'name missing'})
+  }else if  (request.body.number === undefined){
+    return response.status(400).json({error: 'number missing'})
+  }
+  const index = persons.findIndex(person => person.name === request.body.name)
+  if ( index !== -1 ){
+    return response.status(400).json({error: 'name must be unique'})
+  }
+  const person = request.body
+  person.id = Math.floor(Math.random()*10000)
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
